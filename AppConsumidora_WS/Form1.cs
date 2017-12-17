a0ac331013077d6090556f95864375bb6ef97cd5
Pord123 <pedroantbretonca@hotmail.com>
2017-12-16 23:48:12 -0500

    First commit

---
+++ AppConsumidora_WS/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AppConsumidora_WS
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (
+
+Convert.ToString(this.comboBox1.SelectedItem) != ""
+&& Convert.ToString(this.textBox1.Text) != ""
+&& Convert.ToString(this.textBox2.Text) != ""
+
+)
+            {
+                WSOperacionesMatematicas.Operaciones_Matematicas ObjOperacionesMatematicas = new WSOperacionesMatematicas.Operaciones_Matematicas();
+
+                Int32 resultado = 0;
+                this.textBox3.Text = string.Empty;
+
+                switch (Convert.ToString(this.comboBox1.SelectedItem))
+                {
+                    case "Add":
+                        resultado = ObjOperacionesMatematicas.Add(Convert.ToInt32(this.textBox1.Text), Convert.ToInt32(this.textBox2.Text));
+                        break;
+
+                    case "Substract":
+                        resultado = ObjOperacionesMatematicas.Substract(Convert.ToInt32(this.textBox1.Text), Convert.ToInt32(this.textBox2.Text));
+                        break;
+
+                    case "Multiply":
+                        resultado = ObjOperacionesMatematicas.Multiply(Convert.ToInt32(this.textBox1.Text), Convert.ToInt32(this.textBox2.Text));
+                        break;
+
+                    case "Divide":
+                        resultado = ObjOperacionesMatematicas.Divide(Convert.ToInt32(this.textBox1.Text), Convert.ToInt32(this.textBox2.Text));
+                        break;
+
+                }
+
+                this.textBox3.Text = resultado.ToString();
+            }
+        }
+    }
+}
---
+++ AppConsumidora_WS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AppConsumidora_WS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppConsumidora_WS")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eca954f4-cbe0-4ecc-9846-75071102752f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
